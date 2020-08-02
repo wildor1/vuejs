e1b1c12213b344911c12fd85dce82c55a8c07246
wilwildorwildor <wildor@163.com>
2020-08-02 08:58:33 +0800

    全局store

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>This is an about page</h1>
+    <about1></about1>
+  </div>
+</template>
+
+<script>
+import about1 from "./about1.vue";
+export default{
+  
+  components: {
+    about1
+  }
+}
+</script>
---
+++ src/views/about1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>{{ ct }}</div>
+    <button @click="add">jia1 </button>
+  </div>
+</template>
+
+<script>
+import store from "./b.js"; //export default就不用{store}了
+
+export default{
+  data(){
+    var ct=store.state.count
+    return {ct}
+  },
+  computed: {
+    count () {
+      return store.state.count
+    }
+  },
+  methods:{
+    add(){
+     this.ct++
+    }
+  }
+}
+
+</script>
